f2fa55f4f8937c593a5eef789730a03f770b7706
rickylee123329 <rickylee9410@gmail.com>
2025-11-15 10:08:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ creds = Credentials.from_service_account_info(
 gc = gspread.authorize(creds)
 
 # 打開你的 Google Sheet，假設名稱為 "志工受災戶表單"
-sheet = gc.open("disaster-受災戶需求").sheet1
+sheet = gc.open("disasterVictims").sheet1
 
 # ---------- Streamlit 表單 ----------
 st.title("志工與受災戶登記表單")
