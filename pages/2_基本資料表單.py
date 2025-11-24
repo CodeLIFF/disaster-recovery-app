c8159fc578c48df79d2ca4a6bed297e745033d5e
myrnayeh-cloud <b12601004@g.ntu.edu.tw>
2025-11-24 18:15:08 +0800

    Update 2_基本資料表單.py

--- pages/2_基本資料表單.py
+++ pages/2_基本資料表單.py
@@ -5,7 +5,7 @@ from google.oauth2.service_account import Credentials
 
 # ---------- Google Sheet 連線 ----------
 creds = Credentials.from_service_account_info(
-    st.secrets["google"],
+    st.secrets["google_sheet"],
     scopes=[
         "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive",
